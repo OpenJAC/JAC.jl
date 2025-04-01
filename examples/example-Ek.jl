3b4584f112845182487927e0650a4640a756f784
Stephan Fritzsche <s.fritzsche@gsi.de>
2025-04-01 12:39:03 +0200

    x

--- examples/example-Ek.jl
+++
@@ -1,9 +0,0 @@
-
-println("Ek) Test of the PairAnnihilation1Photon module with ASF from an internally generated initial- and final-state multiplet.")
-
-if  true
-    # Last successful:  unknown ...
-    # Compute 
-    #
-end
-
